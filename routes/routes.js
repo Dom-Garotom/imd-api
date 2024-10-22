58dfc95626e257ea5cd6dfe5d14249fdae07df5f
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-10-22 10:55:44 -0300

    feat: retornando as tags dos produtos nas outras rotas do produto

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,44 @@ const express = require("express");
 const MyMildeware = require("../midleware/midleware.js");
 const router = express.Router();
 
-const { Produto, Tag } = require("../models");
+const { Produto, Tag  , ProdutoTags} = require("../models");
 const { where, Model } = require("sequelize");
 
 
 
 router.get('/produtos', async (req, res) => {
-    const produtos = await Produto.findAll();
+    const produtos = await Produto.findAll({
+        include: {
+            model: Tag,
+            attributes: ["nome"],
+            through: { 
+                model: ProdutoTags,
+                attributes: [] 
+            }
+        }
+    });
+
+
+    res.json(produtos);
+});
+
+// rota de enviar produto por id
+
+router.get('/produtos/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const produtos = await Produto.findByPk(id , { 
+        include: {
+            model: Tag,
+            attributes: ["nome"],
+            through: { 
+                model: ProdutoTags,
+                attributes: [] 
+            }
+        }
+    });
+
+
     res.json(produtos);
 });
 
@@ -29,19 +60,30 @@ router.post('/produtos', MyMildeware , async (req, res) => {
     });
 
     if (tags && tags.length > 0){
-        const instancias = await Promise.all ( tags.map( tag => Tag.findOrCreate({ where : {nome : tag} }) ) );
-        novoProduto.addTags(instancias.map( a => a[0]));
+        
+        const instancias = await Promise.all ( tags.map( tag => Tag.findOrCreate({ 
+            where : {nome : tag} , 
+            defaults : { nome: tag} 
+            }) 
+        ));
+    
+        novoProduto.addTags( instancias.map( ([tags , bool]) => tags) );
     }
 
-    const produto = await Produto.findByPk(novoProduto.id , {
+
+    const produto = await Produto.findByPk( novoProduto.id , {
         include: {
             model: Tag,
             attributes: ["nome"],
-            through: { attributes: [] }
+            through: { 
+                model: ProdutoTags,
+                attributes: [] 
+            }
         }
     });
+
     
-    res.status(201).json(produto);
+    res.status(201).json({mensage : "produto criado com sucesso" , produto: produto});
 
 });
 
@@ -53,7 +95,16 @@ router.put('/produtos/:id', MyMildeware , async (req, res) => {
     const id = parseInt(req.params.id);
     const { nome, descricao, preco } = req.body;
 
-    const produto = await Produto.findByPk(id)
+    const produto = await Produto.findByPk(id , {
+        include: {
+            model: Tag,
+            attributes: ["nome"],
+            through: { 
+                model: ProdutoTags,
+                attributes: [] 
+            }
+        }  
+    })
  
     if (!produto) {
         return res.status(404).json({ error: 'Produto não encontrado' });
